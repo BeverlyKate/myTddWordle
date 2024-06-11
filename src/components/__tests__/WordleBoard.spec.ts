5f64cc4a2fc94e341e289fba6321832a19393927
BeverlyKate <129060018+BeverlyKate@users.noreply.github.com>
2024-06-11 19:43:13 +0800

    Extract playerSubmitsGuess() method

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -14,34 +14,29 @@ describe('WordleBoard', () => {
       wrapper= mount(WordleBoard, {props: {wordOfTheDay}})
   })
 
-
-  test("a victory message appears when the user makes a guess that matches the word of the day", async() => {
+  async function playerSubmitsGuess(guess: string){
     //Act phase, player interacts with the game
     const guessInput= wrapper.find("input[type=text]")
-    await guessInput.setValue(wordOfTheDay)
+    await guessInput.setValue(guess)
     await guessInput.trigger("keydown.enter")
+  }
 
+  test("a victory message appears when the user makes a guess that matches the word of the day", async() => {
+   await playerSubmitsGuess(wordOfTheDay)
     //Assert 
     expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
   //testing languages let us set future tests we wanna do:
   test("a defeat message appears if the user makes an incorrect guess", async() =>{
-        // //Arrange phase
-        // const wrapper= mount(WordleBoard, {props: {wordOfTheDay}})
-
-        //Act phase, player interacts with the game
-        const guessInput= wrapper.find("input[type=text]")
-        await guessInput.setValue("WRONG")
-        await guessInput.trigger("keydown.enter")
-    
-        //Assert 
-        expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
+    await playerSubmitsGuess("WRONG")
+    //Assert 
+    expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
   })
 
   test("no end-of-game message appears if the user hes not yet made a guess", async()=> {
-          // //Arrange phase
-          // const wrapper= mount(WordleBoard, {props: {wordOfTheDay}})
-          expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
-          expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
+    // //Arrange phase
+    // const wrapper= mount(WordleBoard, {props: {wordOfTheDay}})
+    expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
+    expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
   })
 })
