9359c5a7968a4c3e741e6f755f25a8e163019ffb
BeverlyKate <129060018+BeverlyKate@users.noreply.github.com>
2024-06-11 17:37:46 +0800

    Render victory message

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 defineProps<{
-  msg: string
+  wordOfTheDay: string
 }>()
 </script>
 
 <template>
-  <p>{{ msg }}</p>
+  <input type="text">
+  <p>{{ wordOfTheDay }}</p>
+  <p>"You won!"</p>
 </template>
--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -4,8 +4,16 @@ import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
 
 describe('WordleBoard', () => {
-  it('renders properly', () => {
-    const wrapper = mount(WordleBoard, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+  test("a victory message appears when the user makes a guess that matches the word of the day", async() => {
+    //Arrange phase
+    const wrapper= mount(WordleBoard, {props: {wordOfTheDay: "TESTS"}})
+
+    //Act phase, player interacts with the game
+    const guessInput= wrapper.find("input[type=text]")
+    await guessInput.setValue("TESTS")
+    await guessInput.trigger("keydown.enter")
+
+    //Assert 
+    expect(wrapper.text()).toContain("You won!")
   })
 })
